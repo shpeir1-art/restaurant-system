e1301f34e28ca09121b57aeaa0f4a3f4f8fa85e0
shpeir1-art <shpeir1@gmail.com>
2026-05-31 14:59:58 +0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,24 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import TablePage from './pages/TablePage'
 import WaiterPage from './pages/WaiterPage'
+import LoginPage from './pages/LoginPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
+
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      <Route path="/login" element={<LoginPage />} />
+
       <Route path="/table/:id" element={<TablePage />} />
+
       <Route path="/waiter" element={<WaiterPage />} />
+
     </Routes>
   </BrowserRouter>
 )
